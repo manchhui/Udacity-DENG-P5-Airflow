20bc12d47db8d5302dca260b767a491544af90ec
Man-Chun Hui <manchhui@gmail.com>
2020-08-28 21:37:08 +0100

    Update load_fact.py
    type: feat
    
    This operator loads the FACT table and parameters are passed to it from the main DAG.

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from helpers import SqlQueries
 
 class LoadFactOperator(BaseOperator):
 
@@ -8,15 +9,21 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table="",
+                 redshift_conn_id="",
+                 sql="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql = sql
+        
     def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
         self.log.info('LoadFactOperator not implemented yet')
+        formatted_sql = getattr(SqlQueries,self.sql)
+        
+        self.log.info('Loading Fact Table')
+        redshift.run(formatted_sql)
