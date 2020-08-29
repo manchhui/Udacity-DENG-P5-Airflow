591eb026ae88d674d7f53e46222262cda0195f08
Man-Chun Hui <manchhui@gmail.com>
2020-08-29 13:16:40 +0100

    Update data_quality.py
    type: feat
    
    Completed the data quality check feature to ensure records were correctly transferred.

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,30 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 tables="",
+                 redshift_conn_id="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
+        self.tables=tables
+        self.redshift_conn_id=redshift_conn_id
+       
     def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         self.log.info('DataQualityOperator not implemented yet')
+        
+        for table in self.tables:
+            self.log.info("Show {} in for loop".format(table))
+            
+            records = redshift.get_records("SELECT COUNT(*) FROM {}".format(table))
+            num_records = records[0][0]
+            
+            if len(records) < 1 or len(records[0]) < 1:
+                logging.error("Data quality check failed. {} returned no results".format(table))
+                raise ValueError("Data quality check failed. {} returned no results".format(table))
+                
+            if num_records == 0:
+                logging.error("No records present in destination table {}".format(table))
+                raise ValueError("No records present in destination table {}".format(table))
+            
+            self.log.info("Data quality on table {} check passed with {} records".format(table, num_records))
